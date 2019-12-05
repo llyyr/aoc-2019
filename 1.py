3e98e8ff0d98409cd10bdcbcc3183f9e514b48a9
llyyr <llyyr@users.noreply.github.com>
2019-12-05 13:07:44 +0530

    fix ugly indent and dir structure

---
+++ 1.py
@@ -0,0 +1,16 @@
+def calculateFuel(i):
+    return int(int(i)/3) - 2
+
+def calculateFuel2(i):
+    add = calculateFuel(i)
+    return (add + calculateFuel2(add)) if add >= 0 else 0
+
+with open("1.in", "r") as f:
+    totalFuelMass = 0
+    totalFuelMass2 = 0
+    for mass in f:
+        totalFuelMass += calculateFuel(mass)
+        totalFuelMass2 += calculateFuel2(mass)
+
+print("Part 1: {}".format(totalFuelMass))
+print("Part 2: {}".format(totalFuelMass2))
---
+++ 2.py
@@ -0,0 +1,23 @@
+with open("2.in", "r") as f:
+    b = [int(x) for x in f.read().split(",")]
+
+
+for x in range(100):
+    for y in range(100):
+        a = [x for x in b]
+        a[1], a[2] = x, y
+        iPos = 0
+
+        while a[iPos] != 99:
+            i1, i2, i3 = a[iPos+1], a[iPos+2], a[iPos+3]
+            if a[iPos] == 1:
+                a[i3] = a[i1] + a[i2]
+            if a[iPos] == 2:
+                a[i3] = a[i1] * a[i2]
+            iPos += 4
+
+        if a[0] == 19690720:
+            print("Part 2: {}".format(100 * x + y))
+
+        if x == 12 and y == 2:
+            print("Part 1: {}".format(a[0]))
