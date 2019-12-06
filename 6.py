b931f589b8d02ce641cab78de817fc47c8784f67
llyyr <llyyr@users.noreply.github.com>
2019-12-06 11:49:46 +0530

    better

--- 6.py
+++ 6.py
@@ -9,4 +9,4 @@ print(sum(len(calc(x)) - 1 for x in tree.keys()))
 
 you, san = calc('YOU'), calc('SAN')
 dist = sum(x == y for x, y in zip(you, san)) + 1
-print(len(you) - you.index(you[dist]) + len(san) - san.index(san[dist]))
+print(len(you + san) - you.index(you[dist]) - san.index(san[dist]))
