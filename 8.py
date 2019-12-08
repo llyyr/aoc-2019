2247563e360c5ce11461386e1b46c8f5a8472886
llyyr <llyyr@users.noreply.github.com>
2019-12-08 12:13:15 +0530

    day 8

---
+++ 8.py
@@ -0,0 +1,15 @@
+f = list(map(int, open('8.in').read().strip()))
+w, h = 25, 6
+wh = w*h
+
+layers = [f[i*wh:(i+1)*wh] for i in range(len(f) // wh)]
+
+l = min(layers, key=lambda l: l.count(0))
+
+print(l.count(1) * l.count(2))
+
+
+img = [[x for x in p if x != 2][0] for p in zip(*layers)]
+pixels = [img[i*w:(i+1)*w] for i in range(h)]
+for row in pixels:
+        print(''.join(' #' [i] for i in row))
